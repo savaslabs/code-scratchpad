3faebf0f2b3dbe63cdb5d210ec14a9c52018a113
Ben Tinsley <benj.tinsley@gmail.com>
2021-11-12 13:08:26 -0700

    adds conditionals and comments to make inital setup easier

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
+/* Feel free to delete the below and add
+ * your own code.
+ * A functional and classical module import
+ * example are provided.
+ * */
 import { functionExample } from "./modules/function-example.js";
 import { ClassExample } from "./modules/class-example.js";
 
 const title = document.querySelector("h1");
 
-functionExample(title);
-new ClassExample(title);
+if (title) {
+  functionExample(title);
+  new ClassExample(title);
+}
