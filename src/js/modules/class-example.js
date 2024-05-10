ba099e24bc215633cce2e9342355479b300113ba
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2024-05-10 12:16:23 -0500

    Updated dependencies (#9)
    * Updated dependencies+
    
    * Deletes .npmrc to prevent blocking install on out of date node/npm versions
    
    ---------
    
    Co-authored-by: Matt Hisamoto <matt.hisamoto@savaslabs.com>

--- src/js/modules/class-example.js
+++ src/js/modules/class-example.js
@@ -1,10 +1,12 @@
 export class ClassExample {
   constructor(el) {
     this.el = el;
-    if (this.el.textContent.length > 0) {
+    if (this.el && this.el.textContent && this.el.textContent.length > 0) {
       console.log(
         `Contents of title from classical component: ${this.el.textContent}`
       );
+    } else {
+      console.log('Class Example was called.')
     }
   }
 }
--- src/js/modules/function-example.js
+++ src/js/modules/function-example.js
@@ -1,7 +1,9 @@
 export function functionExample(el) {
-  if (el.textContent.length > 0) {
+  if (el && el.textContent && el.textContent.length > 0) {
     console.log(
       `Contents of title from functional component: ${el.textContent}`
     );
+  } else {
+    console.log('you definitely called a function, well done.')
   }
 }
